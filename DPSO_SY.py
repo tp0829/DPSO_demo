3e8ca98958f5f4d8d37aeb62716584cf93c2da11
tp0829 <tang100peng@163.com>
2020-04-25 21:08:33 +0800

    0424DPSO

--- DPSO_SY.py
+++ DPSO_SY.py
@@ -1,5 +1,69 @@
 # 离散粒子群优化算法,DPSO
 # 学习笔记
+'''
+3.2 Proposed algorithm//算法
+    - Algorithm 1 函数
+        function Max(Gbest , Gbest)
+            //选择LIE值较大的粒子
+        Degree_Based_Initialization()
+            //初始化函数
+        Local_Search()
+            //局部搜索
+    - Algorithm 1 步骤
+        输入:
+            1.G //图G=(N,E,W)
+            2.n //粒子群的大小
+            3.gmax  //最大迭代次数
+            4.w //惯性权重
+            5.c1,c2 //学习因子
+            6.k //种子集的大小
+        第一步:
+            初始化
+                position vector: 
+                    X ← Degree_Based_Initialization(G,n,k);
+                Pbest vector: 
+                    Pbest ← Degree_Based_Initialization(G,n,k);
+                velocity vector: 
+                    V ← 0;
+        第二步:
+            选择全局最佳位置向量 Gbest
+                According to the fitness value
+        第三步:
+            开始循环,设置g=0;
+                Step3.1 Update V;
+                Step3.2 Update X;
+                Step3.3 Update the Pbest 
+                    and select out the best particle Gbest∗ in the current iteration;
+                Step3.4 Employ the local search operation on Gbest∗: Gbest′ ← Local_Search(Gbest∗); Step3.5 Update the Gbest: Gbest ← Max(Gbest′, Gbest)
+        第四步:
+            for(g=0;g<gmax;g++)
+        输出:
+            best position Gbest as the seed set S.
+
+3.0 算法实现,以Python为例相关库函数
+    - import networkx as nx
+        G = nx.read_edgelist("twitter.txt",create_using=nx.Graph())  #读取文件，构建网络 
+        G.nodes() 
+        G.edges()
+        G.neighbors('0') #注节点是"字符串"型
+
+    - import numpy as np
+        np.random.uniform(-1,1,(10,2)):生成10*2的数组,数组元素大小[-1,1)
+        np.random.rand(10,2):生成10*2的数组,数组元素大小随机默认范围[0,1)
+        np.sum(x,axis=1):axis=0,往里看0层,=1,往里看1层
+        np.argmin(x):返回数组x中最小元素的下标
+        np.mean():求均值
+    - import matplotlib.pyplot as plt
+        plt.figure():
+        plt.show():
+        plt.clf():清除所有轴,窗口打开,重复利用窗口的,已达到动画显示的效果
+    - import random
+        random.sample(list, 2):从列表中随机选择两个元素
+        random.random():生成0和1之间的随机浮点数float
+
+
+'''
+
 
 #!/usr/bin/env python 
 # -*- coding:utf-8 -*-
@@ -11,7 +75,7 @@ import matplotlib.pyplot as plt
 
 def IC_model(g,S,mc=10000,p=0.01):
     spread = []
-    for i in range(mc):
+    for _ in range(mc): # i
         # 模拟传播过程      
         new_active, Au = S[:], S[:]
         while new_active:
@@ -25,7 +89,9 @@ def IC_model(g,S,mc=10000,p=0.01):
             Au += new_active 
         spread.append(len(Au))
     return np.mean(spread)
-      
+
+
+## X 中保存 由种子集定义的 粒子位置
 def SHD(G,all_nodes,K):
     X = [ ] 
     
@@ -36,39 +102,15 @@ def SHD(G,all_nodes,K):
             for ele_2 in random.sample(set(all_nodes)-set(X),1):
                 X[ele_1] = ele_2
     
-    #X = random.sample(all_nodes,K)
     return X
 
+## pop 初始种群大小
 def population_initialization(G,all_nodes,pop,K):
     P = [ ]
-    for ele_9 in range(pop):
+    for _ in range(pop): # ele_9
         P.append(SHD(G,all_nodes,K))
     return P
-'''
-def Eval(G,S,K):
-    one_hop = [ ]
-    two_hop = [ ]
-    one_influence = 0
-    two_influence = 0
-    fitness = 0
-    
-    for i_1 in S:
-        one_hop += G.neighbors(i_1)
-    one_hop = list(set(one_hop)-set(S))
-    
-    for i_2 in one_hop:
-        one_influence += 1 - (1 - 0.01)**len(set(G.neighbors(i_2))&set(S))
-    
-    for j_1 in one_hop:
-        two_hop += G.neighbors(j_1)
-    two_hop = list(set(two_hop) - set(S))
-    
-    for j_2 in two_hop:
-        two_influence += 0.01 * len(set(G.neighbors(j_2))&set(one_hop))  
-    fitness = K + one_influence + (one_influence*two_influence)/len(one_hop)
-    
-    return fitness
-'''   
+
 def Neighbor_Nodeset(G,S):
     neighbors = [ ]  
     for i in S:
@@ -127,7 +169,7 @@ def update_Velocity(Pbest,Gbest,V,X,K,c1,c2,w,r1,r2):
     for i in range(K):
         if X[i] not in intersaction_1:
             key_1.append(i)
-    for j in range(K):
+    for _ in range(K): # j
         V_1.append(0)
     for s in key_1:
         V_1[s] = 1
@@ -135,7 +177,7 @@ def update_Velocity(Pbest,Gbest,V,X,K,c1,c2,w,r1,r2):
     for n in range(K):
         if X[n] not in intersaction_2:
             key_2.append(n)
-    for m in range(K):
+    for _ in range(K): # m
         V_2.append(0)
     for e in key_2:
         V_2[e] = 1
@@ -158,85 +200,81 @@ def update_Position(G,all_nodes,Pbest,Gbest,V,X,K,c1,c2,w,r1,r2):
     return X,V
 
 def main():
-    for address in ["D:\\GrQc.txt","D:\\HepTh.txt","D:\\HepPh.txt","D:\\epinions.txt","D:\\Email-Enron.txt","D:\\gemsec-RO.txt"]:
-        G = nx.read_edgelist(address,create_using=nx.Graph())  #读取文件，构建网络
-        for K in [5,10,15,20,25,30]:
-            B = [ ]
-            C = [ ]
-            for A in range(10):
-                start_time = time.clock()
-                #G = nx.read_edgelist("D:\\Hamsterster full.txt", create_using=nx.Graph())  #读取文件，构建网络 
-                #K = 20
-                pop = 100
-                maxgen = 100
-                c1 = 2.0
-                c2 = 2.0
-                w = 0.8
-                r1 = random.random()
-                r2 = random.random()
-                
-                all_nodes = list(G.nodes())
-                
-                population_dict = { }            #字典，用来存储个体的适应度值
-                P = [ ]
-                P_best = [ ]    
-                P = population_initialization(G,all_nodes,pop,K)  #种群初始化
-                #PSO算法的迭代过程
-                RESULT_1 = [ ]
-                RESULT_2 = [ ]
-                P_best = population_initialization(G,all_nodes,pop,K)    
-                G_best = sorted(P_best,key = lambda x:Evaluation(G,population_dict,x,K),reverse = True)[0]
-                #初始速度
-                Velocity = [ ]
-                for Index_3 in range(pop):
-                    Velocity.append([ ])
-                for Index_1 in range(pop):
-                    for Index_2 in range(K):
-                        Velocity[Index_1].append(0)
-                g = 1
-                while g <= maxgen:
-                    #更新速度和位置
-                    for j in range(len(P)):       
-                        P[j],Velocity[j] = update_Position(G,all_nodes,P_best[j],G_best,Velocity[j],P[j],K,c1,c2,w,r1,r2)
-                    #更新P_best
-                    for i in range(len(P)):
-                        if Evaluation(G,population_dict,P[i],K) >= Evaluation(G,population_dict,P_best[i],K):
-                            P_best[i] = P[i]
-                    #更新G_best
-                    G_best_candidate = sorted(P_best,key = lambda x:Evaluation(G,population_dict,x,K),reverse = True)[0]
-                    
-                    G_best_candidate = local_search(G,all_nodes,G_best_candidate,population_dict,K)
-                    
-                    if Evaluation(G,population_dict,G_best,K) < Evaluation(G,population_dict,G_best_candidate,K):
-                        G_best = G_best_candidate
-                    
-                    diffusion_result = 0
-                    diffusion_result = Evaluation(G,population_dict,G_best,K)
-                    RESULT_1.append(g)
-                    RESULT_2.append(round(diffusion_result,2))
-                    #print('第',g,'次迭代')
-                    
-                    g += 1
-                end_time = time.clock()
-                runningtime = end_time - start_time
-                C.append(runningtime)
-                
-                diffusion_result_list_final = 0
-                diffusion_result_list_final = IC_model(G,G_best)
-                B.append(diffusion_result_list_final)
-                '''
-                plt.figure(num='Figure 12', figsize=(6,6), dpi=75, facecolor='#FFFFFF', edgecolor='#FF0000')
-                plt.xlabel('The Number of Generations')   # x轴标签
-                plt.ylabel('The Value of EDV')  # y轴标签
-                plt.plot(RESULT_1,RESULT_2)
-                plt.legend(loc=4)
-                plt.show()
-                
-                print("总的激活节点数量:",round(diffusion_result_list_final,2)) 
-                print("所用时间：",round(runningtime,2))
-                print(RESULT_1)
-                print(RESULT_2)
-                '''
-            print("网络：",address,"K=",K,"影响力:",round(np.mean(B),1),"时间：",round(np.mean(C),1))
+    #读取文件，构建网络
+    address = "facebook.txt"
+    G = nx.read_edgelist(address,create_using=nx.Graph()) ##图G
+        ## 粒子群的大小
+    maxgen = 100 ##最大迭代次数
+    w = 0.8 ##惯性权重
+    c1 = 2.0 ##学习因子一
+    c2 = 2.0 ##学习因子二
+    K = 10 ##种子集大小
+
+    r1 = random.random()
+    r2 = random.random()
+
+    all_nodes = list(G.nodes())
+
+    B = [ ]
+    C = [ ]
+    population_dict = { }            #字典，用来存储个体的适应度值
+    P = [ ]
+    P_best = [ ]   
+
+    #开始时间
+    start_time = time.clock()
+    pop = 100
+
+    ##种群初始化,位置初始化,每个粒子的位置向量数为 K
+    P = population_initialization(G,all_nodes,pop,K)  
+
+    #PSO算法的迭代过程
+    RESULT_1 = [ ]
+    RESULT_2 = [ ]
+    P_best = population_initialization(G,all_nodes,pop,K)    
+    G_best = sorted(P_best,key = lambda x:Evaluation(G,population_dict,x,K),reverse = True)[0]
+    
+    ##初始速度,全设置为0,每个粒子的速度向量数为 K
+    Velocity = [ ]
+    for _ in range(pop): # Index_3
+        Velocity.append([ ])
+    for Index_1 in range(pop):
+        for _ in range(K): # Index_2
+            Velocity[Index_1].append(0)
+    ## 代数
+    g = 1
+    while g <= maxgen:
+        #更新速度和位置
+        for j in range(len(P)):       
+            P[j],Velocity[j] = update_Position(G,all_nodes,P_best[j],G_best,Velocity[j],P[j],K,c1,c2,w,r1,r2)
+        #更新P_best
+        for i in range(len(P)):
+            if Evaluation(G,population_dict,P[i],K) >= Evaluation(G,population_dict,P_best[i],K):
+                P_best[i] = P[i]
+        #更新G_best
+        G_best_candidate = sorted(P_best,key = lambda x:Evaluation(G,population_dict,x,K),reverse = True)[0]
+        
+        G_best_candidate = local_search(G,all_nodes,G_best_candidate,population_dict,K)
+        
+        if Evaluation(G,population_dict,G_best,K) < Evaluation(G,population_dict,G_best_candidate,K):
+            G_best = G_best_candidate
+        
+        diffusion_result = 0
+        diffusion_result = Evaluation(G,population_dict,G_best,K)
+        RESULT_1.append(g)
+        RESULT_2.append(round(diffusion_result,2))
+        
+        #print('第',g,'次迭代')
+        g += 1
+        end_time = time.clock()
+        
+        runningtime = end_time - start_time
+        C.append(runningtime)
+        
+        diffusion_result_list_final = 0
+        diffusion_result_list_final = IC_model(G,G_best)
+        B.append(diffusion_result_list_final)
+
+        print("网络：",address,"K=",K,"影响力:",round(np.mean(B),1),"时间：",round(np.mean(C),1))
 if __name__ == '__main__':
     main()
